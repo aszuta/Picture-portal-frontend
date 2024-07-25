2dfefdfa17712eaf89218db94f346a695fa57399
unknown <adamszuta@vp.pl>
2024-07-26 00:36:36 +0200

    chore: moved user functions from store

--- plugins/authorization.ts
+++ plugins/authorization.ts
@@ -1,20 +1,50 @@
-import { useUserStore } from "~/store/user";
-import { storeToRefs } from "pinia";
+import { useUserStore } from '~/store/user';
+import { storeToRefs } from 'pinia';
+import { jwtDecode } from 'jwt-decode';
 
 export default defineNuxtPlugin(() => {
     const { isLoggedIn } = storeToRefs(useUserStore());
     const user = useUserStore();
     const cookie = useCookie('authcookie');
-    console.log(cookie.value);
+    const api = useApi();
+
+    const getUser = async () => {
+        const profile = await api('/api/auth/profile', {
+            method: 'GET'
+        });
+        console.log(profile);
+        user.setUser(profile);
+    }
 
     if (cookie.value) {
-        console.log(isLoggedIn.value);
         isLoggedIn.value = true;
+    }
+
+    if (user.$state.isLoggedIn == true) {
+        getUser();
     }
 
     if (import.meta.client) {
         if (user.$state.isLoggedIn == true) {
-            user.intervalToken();
+            setInterval(async () => {
+                const cookie = useCookie('authcookie')?.value;
+                if (!cookie) {
+                    return await $fetch('/api/auth/refresh', {
+                        credentials: 'include',
+                    });
+                }
+    
+                const decodedCookie = jwtDecode(cookie);
+                const expiration = decodedCookie.exp - 30;
+                const expDate = Date.now() / 1000;
+                console.log(cookie);
+                    
+                if (expDate > expiration) {
+                    await $fetch('/api/auth/refresh', {
+                        credentials: 'include',
+                    });
+                }
+            }, 30000);
         }
     }
 });
