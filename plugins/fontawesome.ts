ca8fe253e2968242466800d731334305f3b87d4b
unknown <adamszuta@vp.pl>
2024-09-11 22:01:35 +0200

    chore: fixed auth and fontawesome plugins

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -9,13 +9,14 @@ import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faUser, faHeart, faPlus, faMagnifyingGlass, faXmark, faAngleLeft, faAngleRight, faCalendar, faPaperPlane);
+library.add(faUser, faHeart, faPlus, faMagnifyingGlass, faXmark, faAngleLeft, faAngleRight, faCalendar, faPaperPlane, faUpload);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
